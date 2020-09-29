5cdc5465cc257498577d9faeffd2050f87f0f5d5
Aaron Berberich <aberberich4006@gmail.com>
2020-09-28 20:19:13 -0400

    added base classes

---
+++ GameReviewer.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameReviewer.WebMVC.Startup))]
+namespace GameReviewer.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
