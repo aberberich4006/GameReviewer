865314056e0969f82ce0b877529e43212e029f85
Aaron Berberich <aberberich4006@gmail.com>
2020-09-28 20:43:18 -0400

    Added connection string/database

---
+++ GameReview.Models/Entities/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameReview.Models
+{
+    public class Game
+    {
+        [Key]
+        public int GameId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        [ForeignKey(nameof(Category))]
+        public int CategoryId { get; set;}
+        public virtual Category Category { get; set; }
+
+        public ICollection<UserGameReview> UserGameReviews { get; set; }
+        
+    }
+}
---
+++ GameReview.Models/Entities/UserGameReview.cs
@@ -0,0 +1,33 @@
+﻿using GameReviewer.WebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameReview.Models
+{
+    public class UserGameReview
+    {
+        [Key]
+        public int UserGameReviewId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Game))]
+        public int GameId { get; set; }
+        public virtual Game Game { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(ApplicationUser))]
+        public string UserId { get; set; }
+        
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        [Required]
+        [Range(1,10, ErrorMessage ="Please choose a number between 1 and 10.")]
+        [Display(Name ="Review Score")]
+        public decimal UserReview { get; set; }
+    }
+}
