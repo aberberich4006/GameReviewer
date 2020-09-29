c8f1f4056c7bb4dd9e622dcc71abbc2dc367b0dd
Aaron Berberich <aberberich4006@gmail.com>
2020-09-29 00:40:20 -0400

    Added Game Index View and Game Create view

---
+++ GameReview.Data/GameCreate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameReview.Models
+{
+    public class GameCreate
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name="Category Id")]
+        public int CategoryId { get; set; }
+        [Required]
+        public string Developer { get; set; }
+        [Required]
+        [Display(Name="Release Year")]
+        public int ReleaseYear { get; set; }
+
+    }
+}
--- GameReviewer.WebMVC/Controllers/GameController.cs
+++ GameReviewer.WebMVC/Controllers/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameReview.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,29 @@ using System.Web.Mvc;
 
 namespace GameReviewer.WebMVC.Controllers
 {
+
     public class GameController : Controller
     {
-        // GET: Game
         public ActionResult Index()
         {
+            var model = new GameListItem[0];
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(GameCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
         }
     }
 }
